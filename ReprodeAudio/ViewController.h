ff827157187aaff1651c6fb88b2b0bb4d2cd0487
ElMicroChip <mac_alfil_9@Mac-Alfil-9.local>
2014-08-26 17:46:23 -0500

    Repro de Audio Ej

--- ReprodeAudio/ViewController.h
+++ ReprodeAudio/ViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController <AVAudioPlayerDelegate>
+
+- (IBAction)pLay:(id)sender;
+- (IBAction)sTop:(id)sender;
+- (IBAction)pause:(id)sender;
+- (IBAction)changeVolume:(id)sender;
+- (IBAction)changeSpeed:(id)sender;
+- (IBAction)changeOptions:(id)sender;
+@property (strong, nonatomic) IBOutlet UILabel *labelTime;
+
+@property (strong, nonatomic) IBOutlet UIView *optiopnsContainer;
+
+
+@property (strong, nonatomic) IBOutlet UIProgressView *progressBar;
+
+
+
+
+@property (strong, nonatomic) AVAudioPlayer * reproductor;
+
 
 @end
--- ReprodeAudio/ViewController.m
+++ ReprodeAudio/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSTimer * progressTimer;
+}
 @end
 
 @implementation ViewController
@@ -17,13 +19,84 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	NSError * error;
+    NSString * ruta = [[NSBundle mainBundle] pathForResource:@"musica" ofType:@"mp3"];
+    NSURL * url = [[NSURL alloc] initFileURLWithPath:ruta];
+    
+    
+    NSLog(@"%@", ruta);
+        NSLog(@"%@", url);
+
+    
+    
+    
+    
+    _reproductor = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    
+    
+    _reproductor.volume = 1;
+    _reproductor.pan = 0.5;
+    _reproductor.rate = 1;
+    _reproductor.enableRate = YES;
+    _reproductor.numberOfLoops = -1;
+    _reproductor.delegate = self;
+    [_reproductor prepareToPlay];
+}
+-(NSString*)timeConverter:(float)parameter{
+
+    int hours = parameter / 3600;
+    int minutes = (int)(parameter/60) % 60;
+    int seconds = (int)parameter % 60;
+    return [NSString stringWithFormat:@"%02d:%02d:%02d",hours, minutes, seconds];
+
 }
 
+
+- (void)updateBarrayTiempo{
+    _labelTime.text = [NSString stringWithFormat:@"%@", [self timeConverter:_reproductor.currentTime]];
+    _progressBar.progress = _reproductor.currentTime/_reproductor.duration;
+
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pLay:(id)sender {
+    [_reproductor play];
+    progressTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateBarrayTiempo) userInfo:nil repeats:YES];
+}
+
+- (IBAction)sTop:(id)sender {
+    
+    [_reproductor stop];
+    _reproductor.currentTime = 0;
+    [progressTimer invalidate];
+    _progressBar.progress = 0;
+
+}
+
+- (IBAction)pause:(id)sender {
+    [_reproductor pause];
+}
+
+- (IBAction)changeVolume:(id)sender {
+    UISlider * control = sender;
+    _reproductor.volume = control.value;
+}
+
+- (IBAction)changeSpeed:(id)sender {
+    _reproductor.rate = ((UISlider *)sender).value;
+}
+
+- (IBAction)changeOptions:(id)sender {
+    UISwitch * control = sender;
+    if (control.on) {
+        _optiopnsContainer.hidden = NO;
+    } else {
+        _optiopnsContainer.hidden = YES;
+    }
+    
+}
 @end
